7b7012a489921d96f0bd088b90fb9f35fda57325
Sam Kellett <samkellett@gmail.com>
2014-08-26 16:24:08 +0100

    Fixed typo in DownloadGLCoreARBHeader

--- DownloadGLCoreARBHeader.cmake
+++ DownloadGLCoreARBHeader.cmake
@@ -9,18 +9,18 @@ else()
     message(STATUS "Core Profile API header found in /usr/include")
   else()
     message(STATUS "No Core Profile API header in /usr. Using local one.")
-    if(NOT EXISTS ${CMAKE_BUILD_DIR}/include/GL)
-      execute_process(COMMAND mkdir -p ${CMAKE_BUILD_DIR}/include/GL)
+    if(NOT EXISTS ${CMAKE_BINARY_DIR}/include/GL)
+      execute_process(COMMAND mkdir -p ${CMAKE_BINARY_DIR}/include/GL)
     endif()
 
-    if(NOT EXISTS ${CMAKE_BUILD_DIR}/include/GL/glcorearb.h)
+    if(NOT EXISTS ${CMAKE_BINARY_DIR}/include/GL/glcorearb.h)
       execute_process(
-        WORKING_DIRECTORY ${CMAKE_BUILD_DIR}/include/GL
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/include/GL
         COMMAND wget http://opengl.org/registry/api/glcorearb.h
       )
     endif()
 
-    include_directories(${CMAKE_BUILD_DIR}/include)
+    include_directories(${CMAKE_BINARY_DIR}/include)
   endif()
 endif()
 
