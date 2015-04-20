b23f38baaceeedb5f73716ed458d34e60fa9e2a3
Sam Kellett <samkellett@gmail.com>
2015-04-20 19:50:17 +0100

    Removed ExternalProject from FindMeta.

--- FindMeta.cmake
+++ FindMeta.cmake
@@ -16,21 +16,7 @@ if(NOT EXISTS "${Meta_INCLUDE_DIR}")
 endif()
 
 if(EXISTS "${Meta_INCLUDE_DIR}")
-  include(FindPackageHandleStandardArgs)
   mark_as_advanced(Meta_INCLUDE_DIR)
-else()
-  include(ExternalProject)
-  ExternalProject_Add(meta
-    GIT_REPOSITORY https://github.com/ericniebler/meta.git
-    TIMEOUT 5
-    CMAKE_ARGS -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
-    PREFIX "${CMAKE_CURRENT_BINARY_DIR}"
-    INSTALL_COMMAND "" # Disable install step
-    )
-
-  # Specify include dir
-  ExternalProject_Get_Property(meta source_dir)
-  set(Meta_INCLUDE_DIR ${source_dir}/include)
 endif()
 
 if(EXISTS "${Meta_INCLUDE_DIR}")
